18f9390ed85ca9dd52d8a2ab0ae5b747ae902c98
1546329G <1546329@senati.pe>
2024-08-19 22:04:11 -0500

    videso de tuyu prctica

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+mi_lista = [1,2,3,4,5]
+mi_lista.append(45) #Este método agrega un elemento al final de una lista.
+print (mi_lista)
+
+mi_lista.pop(3) # Este método devuelve el último elemento de la lista, y lo borra de la misma.
+print (mi_lista)
+
+mi_lista.remove(3)
+print (mi_lista)
